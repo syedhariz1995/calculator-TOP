4cda11cd18d369415e1897d8aeca46275f4e47a1
syedhariz1995 <syedhariz1995@gmail.com>
2023-10-31 15:39:28 +0800

    added JS

--- index.js
+++ index.js
@@ -1,116 +1,22 @@
-// // Query selects the buttons
-// const numberButtons = document.querySelectorAll(`[data-number]`)
-// const operationButtons = document.querySelectorAll(`[data-operation]`)
-// const equalsButton = document.querySelector(`[data-equals]`)
-// const deleteButton = document.querySelector(`[data-delete]`)
-// const allClearButton = document.querySelector(`[data-all-clear]`)
-// const previousOperandTextElement= document.querySelector(`[data-previous-operand]`)
-// const currentOperandTextElement = document.querySelector(`[data-current-operand]`)
+const buttons = document.querySelectorAll('button')
+const screenDisplay = document.querySelector('.screen')
 
-// // Initialize variables to store the operands and the operator
-// let currentOperand = '';
-// let previousOperand = '';
-// let operation = undefined;
+let calculation = []
+let accumulativeCalculation
 
-// // Add event listeners to number buttons
-// numberButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         appendNumber(button.innerText);
-//         updateDisplay();
-//     });
-// });
+const calculate = (button) => {
+    const value = button.textContent
 
-// // Add event listeners to operation buttons
-// operationButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         selectOperation(button.innerText);
-//         updateDisplay();
-//     });
-// });
+    if(value === "CLEAR"){
+        calculation = []
+        screenDisplay.textContent = '.'
+    }else if(value === "="){
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else{
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
+}
 
-// // Add event listener to the equals button
-// equalsButton.addEventListener('click', () => {
-//     compute();
-//     updateDisplay();
-// });
-
-// // Add event listener to the AC (clear) button
-// allClearButton.addEventListener('click', () => {
-//     clear();
-//     updateDisplay();
-// });
-
-// // Add event listener to the DEL (delete) button
-// deleteButton.addEventListener('click', () => {
-//     deleteLast();
-//     updateDisplay();
-// });
-
-// // Function to append numbers to the current operand
-// function appendNumber(number) {
-//     currentOperand += number;
-// }
-
-// // Function to handle operator selection
-// function selectOperation(op) {
-//     if (currentOperand === '') return;
-//     if (previousOperand !== '') {
-//         compute();
-//     }
-//     operation = op;
-//     previousOperand = currentOperand;
-//     currentOperand = '';
-// }
-
-// // Function to perform the calculation
-// function compute() {
-//     let computation;
-//     const prev = parseFloat(previousOperand);
-//     const current = parseFloat(currentOperand);
-//     if (isNaN(prev) || isNaN(current)) return;
-//     switch (operation) {
-//         case '+':
-//             computation = prev + current;
-//             break;
-//         case '-':
-//             computation = prev - current;
-//             break;
-//         case '*':
-//             computation = prev * current;
-//             break;
-//         case '÷':
-//             computation = prev / current;
-//             break;
-//         default:
-//             return;
-//     }
-//     currentOperand = computation;
-//     operation = undefined;
-//     previousOperand = '';
-// }
-
-// // Function to clear the calculator
-// function clear() {
-//     currentOperand = '';
-//     previousOperand = '';
-//     operation = undefined;
-// }
-
-// // Function to delete the last digit
-// function deleteLast() {
-//     currentOperand = currentOperand.slice(0, -1);
-// }
-
-// // Function to update the display
-// function updateDisplay() {
-//     currentOperandTextElement.innerText = currentOperand;
-//     if (operation != null) {
-//         previousOperandTextElement.innerText = `${previousOperand} ${operation}`;
-//     } else {
-//         previousOperandTextElement.innerText = '';
-//     }
-// }
-
-
-
-
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
