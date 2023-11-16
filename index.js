9e34b17255bfc901a24e343d35b0909008f028e4
syedhariz1995 <syedhariz1995@gmail.com>
2023-11-16 16:53:10 +0800

    restarting project

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-const screen = document.querySelector('.screen');
-const buttons = document.querySelectorAll('button');
-const clearButton = document.querySelector('#clear');
-const equalsButton = document.querySelector('#equals');
+// const screen = document.querySelector('.screen');
+// const buttons = document.querySelectorAll('button');
+// const clearButton = document.querySelector('#clear');
+// const equalsButton = document.querySelector('#equals');
 
-let currentInput = '';
-let previousInput = '';
-let operation = '';
-let result = ''; // Initialize result as an empty string
+// let currentInput = '';
+// let previousInput = '';
+// let operation = '';
+// let result = ''; // Initialize result as an empty string
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (button.textContent === '+' || button.textContent === '-' || button.textContent === '*' || button.textContent === '/') {
-      operation = button.textContent;
-      previousInput = currentInput;
-      currentInput = '';
-      screen.textContent = previousInput + operation;
-    } else if (button.textContent === '.') {
-      if (currentInput.includes('.')) {
-        return;
-      } else {
-        currentInput += button.textContent;
-      }
-    } else if (button.textContent === '=') {
-      if (operation === '+') {
-        result = parseFloat(previousInput) + parseFloat(currentInput);
-      } else if (operation === '-') {
-        result = parseFloat(previousInput) - parseFloat(currentInput);
-      } else if (operation === '*') {
-        result = parseFloat(previousInput) * parseFloat(currentInput);
-      } else if (operation === '/') {
-        result = parseFloat(previousInput) / parseFloat(currentInput);
-      }
-      operation = '';
-      previousInput = '';
-      screen.textContent = result.toString(); // Update the screen with the result
-    } else {
-      currentInput += button.textContent;
-      screen.textContent = currentInput;
-    }
-  });
-});
+// buttons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     if (button.textContent === '+' || button.textContent === '-' || button.textContent === '*' || button.textContent === '/') {
+//       operation = button.textContent;
+//       previousInput = currentInput;
+//       currentInput = '';
+//       screen.textContent = previousInput + operation;
+//     } else if (button.textContent === '.') {
+//       if (currentInput.includes('.')) {
+//         return;
+//       } else {
+//         currentInput += button.textContent;
+//       }
+//     } else if (button.textContent === '=') {
+//       if (operation === '+') {
+//         result = parseFloat(previousInput) + parseFloat(currentInput);
+//       } else if (operation === '-') {
+//         result = parseFloat(previousInput) - parseFloat(currentInput);
+//       } else if (operation === '*') {
+//         result = parseFloat(previousInput) * parseFloat(currentInput);
+//       } else if (operation === '/') {
+//         result = parseFloat(previousInput) / parseFloat(currentInput);
+//       }
+//       operation = '';
+//       previousInput = '';
+//       screen.textContent = result.toString(); // Update the screen with the result
+//     } else {
+//       currentInput += button.textContent;
+//       screen.textContent = currentInput;
+//     }
+//   });
+// });
 
-clearButton.addEventListener('click', () => {
-  currentInput = '';
-  previousInput = '';
-  operation = '';
-  result = ''; // Reset result to an empty string
-  screen.textContent = '.';
-});
+// clearButton.addEventListener('click', () => {
+//   currentInput = '';
+//   previousInput = '';
+//   operation = '';
+//   result = ''; // Reset result to an empty string
+//   screen.textContent = '.';
+// });
+
+
+
