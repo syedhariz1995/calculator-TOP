403433a4bf898586a09be3a1fc0565c68e273736
syedhariz1995 <syedhariz1995@gmail.com>
2023-11-21 16:59:46 +0800

    started in JS

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const equal = document.querySelector('.equal')
 const clear = document.querySelector('.all-clear')
 const lastEntityClear = document.querySelector('.last-entity-clear')
 
-let displayFirstNum = ''
-let displaySecondNum = ''
+let display1Num = ''
+let display2Num = ''
 let result = null
 let lastOperation = ''
 let isDot = false
+
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if(e.target.innerText === '.' && !isDot){
+            isDot = true
+        }else if(e.target.innerText === "." && isDot){
+            return
+        }
+        display2Num += e.target.innerText
+        display2.innerText = display2Num
+    })
+})
