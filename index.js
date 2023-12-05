990194b49079650daa33ebeff6b71be9c6c676c3
syedhariz1995 <syedhariz1995@gmail.com>
2023-12-05 15:25:52 +0800

    added operations

--- index.js
+++ index.js
@@ -23,6 +23,56 @@ numbers.forEach(number => {
         }
         display2Num += e.target.innerText
         display2.innerText = display2Num
-        
     })
+})
+
+
+operations.forEach(operation => {
+    operation.addEventListener('click', (e) => {
+        if(!display2Num) return
+        isDot = false
+        const operationName = e.target.innerText
+        if(display1Num && display2Num && lastOperation){
+            mathOperation()
+        }else{
+            result = parseFloat(display2Num)
+        }
+        clearVar(operationName)
+        lastOperation = operationName
+    })
+})
+
+const clearVar = (oprName = '') => {
+    display1Num += display2Num + ' ' + oprName + ' '
+    display1.innerText = display1Num
+    display2.innerText = ''
+    display2Num = ''
+    tempResult.innerText = result
+}
+
+
+const mathOperation = () => {
+    if(lastOperation === 'x'){
+        result = parseFloat(result) * parseFloat(display2Num)
+    } else if(lastOperation === '+'){
+        result = parseFloat(result) + parseFloat(display2Num)
+    }else if(lastOperation === '-'){
+        result = parseFloat(result) - parseFloat(display2Num)
+    }else if(lastOperation === '/'){
+        result = parseFloat(result) / parseFloat(display2Num)
+    }else if(lastOperation === '%'){
+        result = parseFloat(result) % parseFloat(display2Num)
+    }
+}
+
+
+equal.addEventListener('click', (e) => {
+    if(!display1Num || !display2Num) return
+    isDot = false
+    mathOperation()
+    clearVar()
+    display2.innerText = result
+    tempResult.innerText = ''
+    display2Num = result
+    display1Num = ''
 })
